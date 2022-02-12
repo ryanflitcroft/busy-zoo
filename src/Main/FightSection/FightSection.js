789ecd756036da2fd018dadb7eb625c402cee844
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-11 20:27:13 -0800

    refactored all rfc to rcc. State manages in constructor as this.state. Still cant figure out how to update state, event handlers not functional atm.

--- src/Main/FightSection/FightSection.js
+++ src/Main/FightSection/FightSection.js
@@ -1,24 +1,50 @@
-import React from 'react';
+// import React from 'react';
 import './FightSection.css';
 
-export default function FightSection({ lionSize, 
-  setLionSize, 
-  tigerSize, 
-  setTigerSize }) {
-  return (
-    <section className='fight'>
-      <div>
-        <span style={{ fontSize: `${lionSize}em` }}>🐜</span>
-        <span style={{ fontSize: `${tigerSize}em` }}>🪰</span>
-      </div>
+import React, { Component } from 'react';
 
-      <div>
-        <button onClick={() => setLionSize(lionSize + 0.25)}>🔬 ➕</button>
-        <button onClick={() => setLionSize(lionSize - 0.25)}>🔬 ➖</button>
-
-        <button onClick={() => setTigerSize(tigerSize + 0.25)}>🔬 ➕</button>
-        <button onClick={() => setTigerSize(tigerSize - 0.25)}>🔬 ➖</button>
-      </div>
-    </section>
-  );
+export default class FightSection extends Component {
+  render() {
+    console.log('||tiger', this.props.tigerSize, 'lion', this.props.lionSize);
+    return (
+      <section className='fight'>
+        <div>
+          <span style={{ fontSize: `${this.props.lionSize}em` }}>🐜</span>
+          <span style={{ fontSize: `${this.props.tigerSize}em` }}>🪰</span>
+        </div>
+      
+        <div>
+          {/* FIXME: how tf do you update state in rcc?? */}
+          <button onClick={() => this.props.setState(this.props.lionSize + 0.25)}>🔬 ➕</button>
+          <button onClick={() => this.props.setState(this.props.lionSize - 0.25)}>🔬 ➖</button>
+      
+          <button onClick={() => this.props.setState(this.props.tigerSize + 0.25)}>🔬 ➕</button>
+          <button onClick={() => this.props.setState(this.props.tigerSize - 0.25)}>🔬 ➖</button>
+        </div>
+      </section>
+    );
+  }
 }
+
+
+// export default function FightSection({ lionSize, 
+//   setLionSize, 
+//   tigerSize, 
+//   setTigerSize }) {
+//   return (
+//     <section className='fight'>
+//       <div>
+//         <span style={{ fontSize: `${lionSize}em` }}>🐜</span>
+//         <span style={{ fontSize: `${tigerSize}em` }}>🪰</span>
+//       </div>
+
+//       <div>
+//         <button onClick={() => setLionSize(lionSize + 0.25)}>🔬 ➕</button>
+//         <button onClick={() => setLionSize(lionSize - 0.25)}>🔬 ➖</button>
+
+//         <button onClick={() => setTigerSize(tigerSize + 0.25)}>🔬 ➕</button>
+//         <button onClick={() => setTigerSize(tigerSize - 0.25)}>🔬 ➖</button>
+//       </div>
+//     </section>
+//   );
+// }
--- src/Main/ParadeSection/Animal.js
+++ src/Main/ParadeSection/Animal.js
@@ -1,7 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-export default function Animal({ animal }) {
-  return (
-    <span className='parade marquee'>{animal}</span>
-  );
+import React, { Component } from 'react';
+
+export default class Animal extends Component {
+  render() {
+    return (
+      <span className='parade marquee'>{this.props.animal}</span>
+    );
+  }
 }
+
+
+// export default function Animal({ animal }) {
+//   return (
+//     <span className='parade marquee'>{animal}</span>
+//   );
+// }

d29a4c27d4665b90155ab14e352f437ce3c12abf
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-11 19:12:54 -0800

    Refactor rfc to rcc for components App, Main.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
 import './App.css';
-import React from 'react';
-import { useState } from 'react';
+// import React from 'react';
+// import { useState } from 'react';
 import Header from './Header/Header';
 import Main from './Main/Main';
 
-function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [lionSize, setLionSize] = useState(3);
-  const [tigerSize, setTigerSize] = useState(3);
-  const [animalParade, setAnimalParade] = useState(['🦧']);
+import React, { Component } from 'react';
 
-  return (
-    <>
-      <Header name='Busy Zoo' />
-      <Main isOpen={isOpen} 
-        setIsOpen={setIsOpen}
-        lionSize={lionSize}
-        setLionSize={setLionSize}
-        tigerSize={tigerSize}
-        setTigerSize={setTigerSize}
-        animalParade={animalParade}
-        setAnimalParade={setAnimalParade} />
-    </>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      lionSize: 3,
+      tigerSize: 3,
+      animalParade: ['🦧']
+    };
+  }
 
-  );
+  render() {
+    return (
+      <>
+        <Header name='Busy Zoo' isOpen={this.state.isOpen}/>
+        <Main isOpen={this.state.isOpen} 
+          // setIsOpen={setIsOpen}
+          lionSize={this.state.lionSize}
+          // setLionSize={setLionSize}
+          tigerSize={this.state.tigerSize}
+          // setTigerSize={setTigerSize}
+          animalParade={this.state.animalParade}
+          // setAnimalParade={setAnimalParade} 
+        />
+      </>
+    );
+  }
 }
 
-export default App;
+
+// function App() {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [lionSize, setLionSize] = useState(3);
+//   const [tigerSize, setTigerSize] = useState(3);
+//   const [animalParade, setAnimalParade] = useState(['🦧']);
+
+//   return (
+//     <>
+//       <Header name='Busy Zoo' />
+//       <Main isOpen={isOpen} 
+//         setIsOpen={setIsOpen}
+//         lionSize={lionSize}
+//         setLionSize={setLionSize}
+//         tigerSize={tigerSize}
+//         setTigerSize={setTigerSize}
+//         animalParade={animalParade}
+//         setAnimalParade={setAnimalParade} />
+//     </>
+
+//   );
+// }
+
+// export default App;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,23 @@
-import React from 'react';
+// import React from 'react';
 import './Header.css';
 
-export default function Header(props) {
-  return (
-    <header>
-      <h1><span>Welcome to </span>{props.name}</h1>
-    </header>
-  );
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1><span>Welcome to </span>{this.props.name}</h1>
+      </header>
+    );
+  }
 }
+
+
+// export default function Header(props) {
+//   return (
+//     <header>
+//       <h1><span>Welcome to </span>{props.name}</h1>
+//     </header>
+//   );
+// }

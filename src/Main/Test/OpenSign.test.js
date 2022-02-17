ecf0d0ac3687032732b3dcd0ab234b659d317f7b
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-17 14:51:59 -0800

    All tests passing for OpenSign.

--- src/Main/Test/OpenSign.test.js
+++ src/Main/Test/OpenSign.test.js
@@ -1,19 +1,26 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import OpenSign from '../OpenSign/OpenSign';
 
-test('renders component OpenSign', () => {
-  render(<OpenSign
-    isOpen={false}
-    setIsOpen={() => true} />);
+test('renders component OpenSign', async () => {
+  let isOpen = false;
+  const { rerender } = render(<OpenSign
+    isOpen={isOpen}
+    setIsOpen={() => {isOpen = !isOpen;}} />);
 
   const linkElement = screen.getByText(/the zoo is/i);
   expect(linkElement).toBeInTheDocument();
 
-  const theZooIs = screen.getByText(/🌜/i);
-  // console.log('----thezoois', theZooIs);
-  expect(theZooIs).toBeInTheDocument(), 'test proves that there will be a p tag with moon emoji textContent when isOpen is false.';
+  const theZooIsClosed = screen.getByText(/🌜/i);
+  expect(theZooIsClosed).toBeInTheDocument(), 'test proves that there will be a p tag with moon emoji textContent when isOpen is false.';
 
   const isOpenButton = screen.getByText(/open the zoo/i);
   expect(isOpenButton).toBeInTheDocument(), 'this test proves there will be a button with open the zoo textContent when isOpen is false.';
-  // console.log('---isopenbutton', isOpenButton);
+
+  fireEvent.click(isOpenButton);
+
+  rerender(<OpenSign
+    isOpen={isOpen}
+    setIsOpen={() => {isOpen = !isOpen;}} />);
+  const theZooIsOpen = await screen.findByText(/🌞/i);
+  expect(theZooIsOpen).toBeInTheDocument(), 'this test proves that isOpenButton onClick updates state for isOpen, on rerender, theZooIsOpen contains sun emoji textContent.';
 });
